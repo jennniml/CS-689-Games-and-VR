99e288f58e5b50935e1b6d64419737efdb423d05
Jennifer Lee <jennniml@gmail.com>
2019-09-04 21:28:43 -0700

    Added script to move paddle

--- HW1 2D Breakout Game/Assets/PaddleDirection.cs
+++ HW1 2D Breakout Game/Assets/PaddleDirection.cs
@@ -4,17 +4,67 @@ using UnityEngine;
 
 public class PaddleDirection : MonoBehaviour
 {
-    //public Transform
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public KeyCode moveLeft = KeyCode.A;
+    public KeyCode moveRight = KeyCode.D;
+
+    public float speed = 8.0f;
+    public float boundX = 5f;
+    public float boundY = 4.75f;
+
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var vel = rb.velocity;
+        if(Input.GetKey(moveUp))
+        {
+            vel.y = speed;
+            vel.x = 0.0f;
+        }
+        else if(Input.GetKey(moveDown))
+        {
+            vel.y = -speed;
+            vel.x = 0.0f;
+        }
+        else if(Input.GetKey(moveLeft))
+        {
+            vel.x = -speed;
+            vel.y = 0.0f;
+        }
+        else if(Input.GetKey(moveRight))
+        {
+            vel.x = speed;
+            vel.y = 0.0f;
+        }
+        rb.velocity = vel;
+
+        // set bound to stop at walls
+        var pos = transform.position;
+        if(pos.y > boundY)
+        {
+            pos.y = boundY;
+        }
+        else if(pos.y < -boundY)
+        {
+            pos.y = -boundY;
+        }
+        else if(pos.x > boundX)
+        {
+            pos.x = boundX;
+        }
+        else if(pos.x < -boundX)
+        {
+            pos.x = -boundX;
+        }
+        transform.position = pos;
     }
 }
