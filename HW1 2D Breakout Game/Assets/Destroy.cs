611070039ca92901e8a522b409b52a0fecee2bda
Jennifer Lee <jennniml@gmail.com>
2019-09-05 18:55:09 -0700

    Added destroy gameObject script

---
+++ HW1 2D Breakout Game/Assets/Destroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Square")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
