d4b738f16baea7fe9f9fbb65a17252998aac79ed
Jennifer Lee <jennniml@gmail.com>
2019-09-07 15:15:06 -0700

    Created scarlet and grey breaks

--- HW1 2D Breakout Game/Assets/Scripts/Destroy.cs
+++ HW1 2D Breakout Game/Assets/Scripts/Destroy.cs
@@ -7,7 +7,11 @@ public class Destroy : MonoBehaviour
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.name == "Square")
+        if (col.gameObject.tag == "Grey")
+        {
+            Destroy(col.gameObject);
+        }
+        if (col.gameObject.tag == "Scarlet")
         {
             Destroy(col.gameObject);
         }
