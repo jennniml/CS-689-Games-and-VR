0e102d0b318b4f06ca49348f093a5ef94ddda151
Jennifer Lee <jennniml@gmail.com>
2019-09-08 19:01:08 -0700

    Added score, number of remaining grey and scarlet breaks

---
+++ HW1 2D Breakout Game/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+	public TextUpdateUI ui;
+    
+    public int greyNum = 84;
+    public int scarletNum = 41;
+
+    private void Start()
+    {
+		ui = GameObject.FindWithTag("ui").GetComponent<TextUpdateUI>();
+    }
+
+    // Grey or Scarlet Break dissapears when hit by ball
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Grey")
+        {
+            
+            greyNum--;
+            ui.calculateScore(greyNum, scarletNum);
+            Destroy(col.gameObject);
+        }
+        if (col.gameObject.tag == "Scarlet")
+        {
+            
+            scarletNum--;
+            ui.calculateScore(greyNum, scarletNum);
+            Destroy(col.gameObject);
+        }
+
+    }
+
+    
+}
---
+++ HW1 2D Breakout Game/Assets/Scripts/TextUpdateUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextUpdateUI : MonoBehaviour
+{
+	int score;  // score of game
+    public Text scoreText, greyText, scarletText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+	// Calculates the score based on remaining breaks
+	public void calculateScore(int greyNum, int scarletNum)
+	{
+		if (greyNum > scarletNum * 2)
+		{
+			score += 2;
+		}
+		else
+		{
+			score++;
+		}
+        scoreText.text = "Score: " + score;
+		greyText.text = "Grey: " + greyNum;
+		scarletText.text = "Scarlet: " + scarletNum;
+	}
+}
