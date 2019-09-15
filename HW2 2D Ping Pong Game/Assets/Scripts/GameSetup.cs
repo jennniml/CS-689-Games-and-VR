f471fae7e5cbfcc6a840189003079a1dbafffdca
Jennifer Lee <jennniml@gmail.com>
2019-09-15 15:06:32 -0700

    Set paddle bounds to top/bottom wall

--- HW2 2D Ping Pong Game/Assets/Scripts/GameSetup.cs
+++ HW2 2D Ping Pong Game/Assets/Scripts/GameSetup.cs
@@ -9,28 +9,30 @@ public class GameSetup : MonoBehaviour
     public Transform playerLeft, playerRight;
 
     // Start is called before the first frame update
-    void Update()
+    void Awake()
     {
         // Move each wall to edge location
         topWall.size = new Vector2(mainCam.ScreenToWorldPoint(
-            new Vector3(Screen.width * 1.5f, 0f, 0f)).x, 1f);
+            new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f);
         topWall.offset = new Vector2(0f, mainCam.ScreenToWorldPoint(
             new Vector3(0f, Screen.height, 0f)).y + 0.5f);
 
         bottomWall.size = new Vector2(mainCam.ScreenToWorldPoint(
-            new Vector3(Screen.width * 1.5f, 0f, 0f)).x, 1f);
+            new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f);
         bottomWall.offset = new Vector2(0f, mainCam.ScreenToWorldPoint(
             new Vector3(0f, 0f, 0f)).y - 0.5f);
 
         leftWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(
-            new Vector3(0f, Screen.height * 1.5f, 0f)).y);
+            new Vector3(0f, Screen.height * 2f, 0f)).y);
         leftWall.offset = new Vector2(mainCam.ScreenToWorldPoint(
             new Vector3(0f, 0f, 0f)).x - 0.5f, 0f);
 
         rightWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(
-            new Vector3(0f, Screen.height * 1.5f, 0f)).y);
+            new Vector3(0f, Screen.height * 2f, 0f)).y);
         rightWall.offset = new Vector2(mainCam.ScreenToWorldPoint(
             new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
     }
+
+
 
 }
--- HW2 2D Ping Pong Game/Assets/Scripts/PaddleControl.cs
+++ HW2 2D Ping Pong Game/Assets/Scripts/PaddleControl.cs
@@ -11,6 +11,9 @@ public class PaddleControl : MonoBehaviour
     private KeyCode rightMoveDown = KeyCode.DownArrow;
     private float speed = 10.0f;
 
+    public BoxCollider2D topWall, bottomWall;
+    private float topBound = 4, bottomBound = -4;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +26,9 @@ public class PaddleControl : MonoBehaviour
     {
         movePaddle(leftPaddle, leftMoveUp, leftMoveDown);
         movePaddle(rightPaddle, rightMoveUp, rightMoveDown);
+
+        boundPaddle(leftPaddle);
+        boundPaddle(rightPaddle);
     }
 
     // Moves the left and right paddle up/down
@@ -40,5 +46,20 @@ public class PaddleControl : MonoBehaviour
         {
             p.velocity = new Vector2(0, 0);
         }
+    }
+
+    // Set bound of paddle to stop at top/bottom wall
+    void boundPaddle(Rigidbody2D p)
+    {
+        var pos = p.transform.position;
+        if (pos.y > topBound)
+        {
+            pos.y = topBound;
+        }
+        else if (pos.y < bottomBound)
+        {
+            pos.y = bottomBound;
+        }
+        p.transform.position = pos;
     }
 }
