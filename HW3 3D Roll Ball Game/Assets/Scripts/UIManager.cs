f60fdaa7d003e18544bb07be0403f5e58b9e4fe1
Jennifer Lee <jennniml@gmail.com>
2019-09-21 15:31:31 -0700

    Connected load scenes

---
+++ HW3 3D Roll Ball Game/Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Reloads the Level
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+
+    // Loads inputted scene
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+        if (scene == "GameScene")
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    // Exits the game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
