6569086399a664b2d9c5cd8d935e1aa6af9f3707
Jennifer Lee <jennniml@gmail.com>
2019-11-02 15:41:22 -0700

    Added first person & back camera views

---
+++ HW8 Flight Simulator/Assets/CameraView.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraView : MonoBehaviour
+{
+    public Camera firstPersonCamera;
+    public Camera overheadCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPersonCamera.enabled = true;
+        overheadCamera.enabled = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ShowOverheadView();
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            ShowFirstPersonView();
+        }
+    }
+
+    public void ShowOverheadView()
+    {
+        firstPersonCamera.enabled = false;
+        overheadCamera.enabled = true;
+    }
+
+    public void ShowFirstPersonView()
+    {
+        firstPersonCamera.enabled = true;
+        overheadCamera.enabled = false;
+    }
+}
