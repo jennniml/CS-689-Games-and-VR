40183699f598c34474db80bcb930f4509aba2c56
Jennifer Lee <jennniml@gmail.com>
2019-11-10 13:48:34 -0800

    Added aileron, rudder, elevator, new terrain

---
+++ HW8 Flight Simulator/Assets/Scripts/PlaneControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Plane control attatched to " + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(-Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+    }
+}
